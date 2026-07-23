7650ff18afbd043621ad66198d97642abe3eccee
Vicente Castro <vicentecastroibarra@gmail.com>
2026-07-23 03:44:55 -0400

    style: agrandar aun mas la imagen del Manifiesto

--- components/sections/Manifiesto.tsx
+++ components/sections/Manifiesto.tsx
@@ -6,7 +6,7 @@ import FadeInOnScroll from "@/components/shared/FadeInOnScroll";
 export default function Manifiesto() {
   return (
     <section className="px-6 py-24 lg:px-12 lg:py-36">
-      <FadeInOnScroll className="mx-auto max-w-5xl text-center">
+      <FadeInOnScroll className="mx-auto max-w-6xl text-center">
         <SectionBadge label="Manifiesto · I" />
 
         <h2 className="mx-auto text-h2-lg text-navy">
@@ -30,12 +30,12 @@ export default function Manifiesto() {
           Línea predecible de tu realidad conocida
         </p>
 
-        <div className="relative mx-auto mt-6 aspect-[1344/768] w-full max-w-4xl">
+        <div className="relative mx-auto mt-6 aspect-[1344/768] w-full max-w-6xl">
           <Image
             src="/images/manifiesto-diagrama.png"
             alt="Diagrama: entre el pasado (programa automático) y el futuro (programa automático) está el momento presente, donde puede ocurrir un evento desconocido que te cambia."
             fill
-            sizes="(min-width: 1024px) 896px, 90vw"
+            sizes="(min-width: 1280px) 1152px, 95vw"
             className="object-contain"
           />
 
